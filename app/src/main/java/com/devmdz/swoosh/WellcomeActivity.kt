f3148e4a5a9fead8dbfd54916f711e64d87d28c2
iscmdz <iscmdz@hotmail.com>
2018-06-23 09:30:41 -0500

    initial commit

---
+++ app/src/main/java/com/devmdz/swoosh/WellcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.devmdz.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WellcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wellcome)
+    }
+}
